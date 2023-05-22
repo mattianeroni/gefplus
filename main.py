2a7885ac716bd526f0393e981e3f6aa66c1cc6f4
Mattia Neroni <mattianeroni@MacBook-Air-di-Mattia.local>
2023-05-23 00:19:55 +0200

    table

--- main.py
+++ main.py
@@ -4,44 +4,29 @@ import numpy as np
 import matplotlib.pyplot as plt 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+
 import PySimpleGUI as sg
-
-
-
-
-
-browsing_column = [
-    [
-        sg.Text("File Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
-    ],
-    [
-        sg.Listbox(values=[], enable_events=True, size=(40, 20), key="-FILE LIST-")
-    ],
-]
-
-file_preview_column = [
-    [sg.Text("File Preview")],
-    #[sg.Text(size=(40, 1), key="-TOUT-")],
-    #[sg.Image(key="-IMAGE-")],
-    [sg.Table(values=[], headings=[], size=(40,21), auto_size_columns=True, enable_events=True, key="-PREVIEW-")],
-]
-
-
-
-layout = [
-    [
-        sg.Column(browsing_column),
-        sg.VSeperator(),
-        sg.Column(file_preview_column),
-    ]
-]
-
-# Create the window
-window = sg.Window("Surpy", layout)
 #sg.theme("DarkBlue3")
 #sg.set_options(font=("Courier New", 16))
+
+import layout
+
+
+
+def update_preview(window, **table_options):
+
+    if window is not None:
+        window.close()
+
+    new_layout = layout.new()
+    new_layout[1] = [sg.Table(**table_options, key="-PREVIEW-")]
+    return sg.Window("GEF+", new_layout, finalize=True)
+
+
+
+# Create the window
+window = sg.Window("GEF+", layout.new(), finalize=True)
+
 
 # Create an event loop
 while True:
@@ -52,29 +37,10 @@ while True:
         break
 
     # Show files in the selected folder
-    elif event == "-FOLDER-":
-        folder = values["-FOLDER-"]
-        file_list = os.listdir(folder)
+    elif event == "-BROWSER-":
+        filename = values["-BROWSER-"]
+        df = pd.read_csv(filename, index_col=0)
+        window = update_preview(window, headings=df.columns.values.tolist(), values=df.values.tolist())
 
-        fnames = [
-            filename
-        for filename in file_list
-        if os.path.isfile(os.path.join(folder, filename)) and filename.lower().endswith((".png",".csv", ".txt"))
-        ]
-        window["-FILE LIST-"].update(fnames)
-
-    elif event == "-FILE LIST-":  # A file was chosen from the listbox
-        try:
-            filename = os.path.join(values["-FOLDER-"], values["-FILE LIST-"][0])
-            df = pd.read_csv(filename, index_col=0)
-            new_table = sg.Table(values=df.values.tolist(), headings=df.columns.values.tolist(), size=(40,21), auto_size_columns=True, enable_events=True, key="-PREVIEW-")
-            #new_table = sg.Table(values=, headings=[], size=(40,21), auto_size_columns=True, enable_events=True, key="-PREVIEW-")
-            window["-PREVIEW-"].update(new_table) #values=df.values.tolist(), headings=df.columns.values.tolist())
-            #values=df.values.tolist(), heaings=df.columns.values.tolist())
-            #åwindow.refresh()
-        except:
-            pass
-
-    
 
 window.close()
