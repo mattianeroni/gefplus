318e2a1fc87a426b0f184b1ab42d8d2f8d2c2afc
mattia.neroni <mattia.neroni@ahead-research.com>
2023-09-07 10:06:00 +0200

    fix bug icons compilation

--- build_win.py
+++ build_win.py
@@ -1,10 +1,6 @@
 import os 
+
 os.system("pyinstaller " +
 "-F  --onefile --noconsole --icon=logosmall.ico " +
 "--add-data=gefplus/static/*.png;static " + 
-#"--add-data=gefplus/static/export.png;. " + 
-#"--add-data=gefplus/static/autofill.png;. " + 
-#"--add-data=gefplus/static/logo.jpeg;. " + 
-#"--add-data=gefplus/static/logosmall.png;. " + 
-#"--add-data=gefplus/static/settings.png;. " + 
 "main.py")
--- gefplus/ui/absolute_path.py
+++ gefplus/ui/absolute_path.py
@@ -3,6 +3,6 @@ import sys
 
 
 
-def absolutePath(relative_path):
+def absolute_path(relative_path):
     base_path=getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__))) 
     return os.path.join(base_path, relative_path)
