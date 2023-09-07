ac5ed685f49c1fe870602c8879369a5bc99e1d17
mattia.neroni <mattia.neroni@ahead-research.com>
2023-09-07 09:55:37 +0200

    fix bug icons compilation

--- build_win.py
+++ build_win.py
@@ -1,10 +1,10 @@
 import os 
-os.system("pyinstaller" +
-" -F  --onefile --noconsole --icon=logosmall.ico" +
-" --add-data=gefplus/static/import.png;. " + 
-" --add-data=gefplus/static/export.png;. " + 
-" --add-data=gefplus/static/autofill.png;. " + 
-" --add-data=gefplus/static/logo.jpeg;. " + 
-" --add-data=gefplus/static/logosmall.png;. " + 
-" --add-data=gefplus/static/settings.png;. " + 
+os.system("pyinstaller " +
+"-F  --onefile --noconsole --icon=logosmall.ico " +
+"--add-data=gefplus/static/*.png;static " + 
+#"--add-data=gefplus/static/export.png;. " + 
+#"--add-data=gefplus/static/autofill.png;. " + 
+#"--add-data=gefplus/static/logo.jpeg;. " + 
+#"--add-data=gefplus/static/logosmall.png;. " + 
+#"--add-data=gefplus/static/settings.png;. " + 
 "main.py")
